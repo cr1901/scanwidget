5b1beeadfca000d4a672582576dc8ea3e7712498
William D. Jones <thor0505@comcast.net>
2015-12-15 07:15:10 -0500

    Add triangle sliders w/o constrained movement or scale invariance.

--- scanwidget.py
+++ scanwidget.py
@@ -1,3 +1,5 @@
+import math
+
 from PyQt5 import QtGui, QtCore, QtWidgets
 
 # ScanAxis consists of a horizontal line extending indefinitely, major and
@@ -15,6 +17,10 @@ class ScanAxis(QtWidgets.QGraphicsWidget):
     def paint(self, painter, op, widget):
         pass
 
+
+# class ScanModel(QO
+
+
 # ScanScene holds all the objects.
 class ScanScene(QtWidgets.QGraphicsScene):
     def __init__(self):
@@ -31,12 +37,69 @@ class ScanScene(QtWidgets.QGraphicsScene):
 
 # Check for mouse events over the widget. If shift pressed and mouse click,
 # change signal/event propogation to alter the number of points over which
-# to scan.
+# to scan. This widget should intercept mouse events/prevent events from
+# reaching ScanSlider if shift is pressed.
 class ScanBox(QtWidgets.QWidget):
     pass
 
-# The ScanWidget proper 
+
+# Needs to be subclassed from QGraphicsItem b/c Qt will not send mouse events
+# to GraphicsItems that do not reimplement mousePressEvent (How does Qt know?).
+# Qt decides this while iterating to find which GraphicsItem should get control
+# of the mouse. mousePressEvent is accepted by default.
+# TODO: The slider that was most recently clicked should have z-priority.
+# Qt's mouseGrab logic should implement this correctly.
+class ScanSlider(QtWidgets.QGraphicsItem):
+    def __init__(self, px_size = 20):
+        QtWidgets.QGraphicsItem.__init__(self)
+        self.setFlag(QtWidgets.QGraphicsItem.ItemIsMovable, True)
+        
+        # Make slider an equilateral triangle w/ sides px_size pixels wide.
+        altitude = math.ceil((px_size/2)*math.sqrt(3))
+        
+        points = [QtCore.QPoint(-(px_size/2), altitude), QtCore.QPoint(0, 0), \
+            QtCore.QPoint((px_size/2), altitude)]
+        self.shape = QtGui.QPolygon(points)
+
+    def boundingRect(self):
+        pen_width = 1
+        # If bounding box does not cover whole polygon, trails will be left
+        # when the object is moved.
+        return QtCore.QRectF(-10 - pen_width/2, 0 - pen_width / 2, \
+            20 + pen_width, 20 + pen_width)
+
+    def paint(self, painter, op, widget):
+        painter.drawConvexPolygon(self.shape)
+
+    def mousePressEvent(self, ev):
+        QtWidgets.QGraphicsItem.mousePressEvent(self, ev)
+
+    def mouseMoveEvent(self, ev):
+        QtWidgets.QGraphicsItem.mouseMoveEvent(self, ev)
+
+# Is it scene's responsibility or item's responsibility to ensure item stays
+# in range of allowed moves?
+    def itemChange(self, change, val):
+        if change == QtWidgets.QGraphicsItem.ItemPositionChange:
+            print("Position changed")
+        else:
+            print(change)
+
+        return QtWidgets.QGraphicsItem.itemChange(self, change, val)
+
+# The ScanWidget proper.
+# TODO: Scene is centered on visible items by default when the scene is first
+# viewed. We do not want this here; viewed portion of scene should be fixed.
+# Items are moved/transformed within the fixed scene.
 class ScanWidget(QtWidgets.QGraphicsView):
     def __init__(self):
         self.scene = ScanScene()
         QtWidgets.QGraphicsView.__init__(self, self.scene)
+        self.min_slider = self.scene.addItem(ScanSlider()) # , \
+            # brush = QtGui.QBrush(QtGui.QColor(255,0,0,255)))
+        self.max_slider = self.scene.addItem(ScanSlider()) # , \
+            # brush = QtGui.QBrush(QtGui.QColor(0,0,255,255)))
+        # self.max_slider = ScanSlider()        
+        
+        # self.min_slider = ScanSlider()
+        # self.max_slider = ScanSlider()
