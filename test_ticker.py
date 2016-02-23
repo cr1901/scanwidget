fa29243dbf2c3be74668846caf3187a46f1e3219
Robert Jordens <jordens@gmail.com>
2016-02-23 13:16:21 +0100

    ticker: refactor tests, update docs

---
+++ test_ticker.py
@@ -0,0 +1,78 @@
+import unittest
+import numpy as np
+
+from ticker import Ticker
+
+
+class TickTest(unittest.TestCase):
+    def test_some(self):
+        for a, b, n in [
+                (0, 1, 2),
+                (1, 2, 2),
+                (0, 1, 3),
+                (-1, 1, 2),
+                (1e-9, 1.1e-9, 2),
+                (.3, .6, 2),
+                (.3, .6, 3),
+                (.3, .6, 4),
+        ]:
+            self._one(a, b, n)
+
+    def _a(self):
+        for a in [0, np.pi, 1, 2, 1e-6, 1e6, 1e-9, 1e9]:
+            for da in [1.7e-6, 1.3e6]:
+                for ra in [1.5e-6, .1]:
+                    for a in -a, a:
+                        for a in a, a + da, a - da:
+                            for a in a, a*(1 + ra), a*(1 - ra):
+                                yield a
+
+    def _b(self, a):
+        for b in (np.pi, 1, 1.1e-6, 1.9e6):
+            for b in (0, b, a*b):
+                yield b
+
+    def test_many(self):
+        for a in self._a():
+            for b in self._b(a):
+                for n in (2, 3, 4, 10):
+                    if a >= b:
+                        continue
+                    if np.all(np.isfinite((a, b))):
+                        self._one(a, b, n)
+
+    def _one(self, a, b, n=2, d=3):
+        eps = 1e-8
+        with self.subTest(a=a, b=b, n=n, d=d):
+            t = Ticker(n, d)
+            i = t.step(b - a)
+            self.assertGreaterEqual((b - a)/i, t.min_ticks)
+            j = t.ticks(a, b)
+            self.assertGreaterEqual(j[0] + i*eps, a)
+            self.assertLessEqual(j[-1] - i*eps, b)
+            self.assertLess(j[0] - i*(1 + eps), a)
+            self.assertGreater(j[-1] + i*(1 + eps), b)
+            self.assertGreaterEqual(len(j), t.min_ticks)
+            self.assertLessEqual(len(j), np.ceil(t.min_ticks*5/2))
+            # max step ratio
+            o = t.offset(j[0], j[-1])
+            q = j - o
+            m = t.magnitude(q[0], q[-1])
+            q = q/m
+            self.assertLess(abs(q[0]/(q[1] - q[0])),
+                            10**(t.precision + 1))
+            if o:
+                self.assertGreater(abs(j[0]/(j[1] - j[0])),
+                                   10**t.precision)
+                self.assertGreater(q[0] + i*eps/m, 0)
+            ticks, prefix, labels = t(a, b)
+            self.assertEqual(sorted(set(labels)), sorted(labels))
+            v = [eval((prefix + l).replace("−", "-").replace("×", "*"))
+                 for l in labels]
+            np.testing.assert_allclose(ticks, v,
+                                       rtol=10**-t.precision,
+                                       atol=1e-13*i)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- ticker.py
+++ ticker.py
@@ -1,31 +1,33 @@
 # Robert Jordens <rj@m-labs.hk>, 2016
 
 import numpy as np
-import unittest
 
 
 class Ticker:
     # TODO: if this turns out to be computationally limiting, then refactor
     # such that all the log()s and intermediate values are reused. But
     # probably the string formatting itself is the limiting factor here.
-    def __init__(self, min_ticks=3, precision=3,
-                 steps=(5, 2, 1, .5)):
-        # the .5 in steps catches rounding errors where the calculation
-        # of step_magnitude falls into the wrong exponent bin
-        self.min_ticks = min_ticks  # minimum number of ticks
-        # the maximum number of ticks is
-        # max(consecutive ratios in steps)*min_ticks
-        # thus 5/2*min_ticks for default steps
+    def __init__(self, min_ticks=3, precision=3, steps=(5, 2, 1, .5)):
+        """
+        min_ticks: minimum number of ticks to generate
+            The maximum number of ticks is
+            max(consecutive ratios in steps)*min_ticks
+            thus 5/2*min_ticks for default steps.
+        precision: maximum number of significant digits in labels
+            Also extract common offset and magnitude from ticks
+            if dynamic range exceeds precision number of digits
+            (small range on top of large offset).
+        steps: tick increments at a given magnitude
+            The .5 catches rounding errors where the calculation
+            of step_magnitude falls into the wrong exponent bin.
+        """
+        self.min_ticks = min_ticks
         self.precision = precision
-        # maximum number of significant digits in labels
-        # also extract common offset and magnitude from ticks
-        # if dynamic range exceeds precision number of digits
-        # (small range on top of large offset)
-        self.steps = steps  # tick increments at a given magnitude
+        self.steps = steps
 
     def step(self, i):
         """
-        Return recommended step value for interval size i.
+        Return recommended step value for interval size `i`.
         """
         assert i > 0
         step = i/self.min_ticks  # rational step size for min_ticks
@@ -38,7 +40,7 @@ class Ticker:
 
     def ticks(self, a, b):
         """
-        Return recommended tick values for interval [a, b[.
+        Return recommended tick values for interval `[a, b[`.
         """
         step = self.step(b - a)
         a0 = np.ceil(a/step)*step
@@ -63,7 +65,7 @@ class Ticker:
 
     def magnitude(self, a, b):
         """
-        Determine the scaling magnitude of the interval [a, b[.
+        Determine the scaling magnitude.
 
         Return scaling factor that differs from unity if the numbers
         are to be represented as `magnitude * value`.
@@ -89,7 +91,7 @@ class Ticker:
     def compact_exponential(self, v):
         """
         Format `v` in in compact exponential, stripping redundant elements
-        (plusses, leading and trailing zeros, trailing `e`).
+        (pluses, leading and trailing zeros and decimal point, trailing `e`).
         """
         # this is after the matplotlib ScalarFormatter
         # without any i18n
@@ -115,7 +117,7 @@ class Ticker:
     def __call__(self, a, b):
         """
         Determine step size, ticks, offset and magnitude given the interval
-        [a, b[.
+        `[a, b[`.
 
         Return array with tick value, prefix string to be show to the left or
         above the tick labels, and tick labels.
@@ -129,77 +131,3 @@ class Ticker:
         format = self.format(t[1] - t[0])
         labels = [self.fix_minus(format.format(t)) for t in t]
         return ticks, prefix, labels
-
-
-class TickTest(unittest.TestCase):
-    def test_some(self):
-        for a, b, n in [
-                (0, 1, 2),
-                (1, 2, 2),
-                (0, 1, 3),
-                (-1, 1, 2),
-                (1e-9, 1.1e-9, 2),
-                (.3, .6, 2),
-                (.3, .6, 3),
-                (.3, .6, 4),
-        ]:
-            self._one(a, b, n)
-
-    def _a(self):
-        for a in [0, np.pi, 1, 2, 1e-6, 1e6, 1e-9, 1e9]:
-            for da in [1.7e-6, 1.3e6]:
-                for ra in [1.5e-6, .1]:
-                    for a in -a, a:
-                        for a in a, a + da, a - da:
-                            for a in a, a*(1 + ra), a*(1 - ra):
-                                yield a
-
-    def _b(self, a):
-        for b in (np.pi, 1, 1.1e-6, 1.9e6):
-            for b in (0, b, a*b):
-                yield b
-
-    def test_many(self):
-        for a in self._a():
-            for b in self._b(a):
-                for n in (2, 3, 4, 10):
-                    if a >= b:
-                        continue
-                    if np.all(np.isfinite((a, b))):
-                        self._one(a, b, n)
-
-    def _one(self, a, b, n=2, d=3):
-        eps = 1e-8
-        with self.subTest(a=a, b=b, n=n, d=d):
-            t = Ticker(n, d)
-            i = t.step(b - a)
-            self.assertGreaterEqual((b - a)/i, t.min_ticks)
-            j = t.ticks(a, b)
-            self.assertGreaterEqual(j[0] + i*eps, a)
-            self.assertLessEqual(j[-1] - i*eps, b)
-            self.assertLess(j[0] - i*(1 + eps), a)
-            self.assertGreater(j[-1] + i*(1 + eps), b)
-            self.assertGreaterEqual(len(j), t.min_ticks)
-            self.assertLessEqual(len(j), np.ceil(t.min_ticks*5/2))
-            # max step ratio
-            o = t.offset(j[0], j[-1])
-            q = j - o
-            m = t.magnitude(q[0], q[-1])
-            q = q/m
-            self.assertLess(abs(q[0]/(q[1] - q[0])),
-                            10**(t.precision + 1))
-            if o:
-                self.assertGreater(abs(j[0]/(j[1] - j[0])),
-                                   10**t.precision)
-                self.assertGreater(q[0] + i*eps/m, 0)
-            ticks, prefix, labels = t(a, b)
-            self.assertEqual(sorted(set(labels)), sorted(labels))
-            v = [eval((prefix + l).replace("−", "-").replace("×", "*"))
-                 for l in labels]
-            np.testing.assert_allclose(ticks, v,
-                                       rtol=10**-t.precision,
-                                       atol=1e-13*i)
-
-
-if __name__ == "__main__":
-    unittest.main()
